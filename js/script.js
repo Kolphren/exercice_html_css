4b7b59d65432c3e22b281379d5172111cc5e8dc5
Lionnel <tlionnel@gmail.com>
2020-12-07 15:30:55 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var basket = 0;
 
+// fonction pour ajouter un chiffre au panier virtuel
 function addBasket(){
     basket ++;
     document.getElementById('basket').innerHTML = ('Panier : ' + basket);
 }
 
+// sélection des boutons "ajouter au panier"
 var buttons = document.querySelectorAll('#add');
-    buttons.forEach(element => {
-        element.addEventListener('click', addBasket);
-    });
+buttons.forEach(element => {
+    element.addEventListener('click', addBasket);
+});
